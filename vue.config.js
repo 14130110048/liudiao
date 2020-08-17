4fb22de26a145e995765ead8e71471ff130e6694
wangxin <553613117@qq.com>
2020-08-17 15:31:56 +0800

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+const webpack = require('webpack')
+module.exports = {
+  lintOnSave: false,
+  baseUrl: './',
+  configureWebpack: {
+
+    plugins: [
+
+      new webpack.ProvidePlugin({
+
+        $: 'jquery',
+
+        jQuery: 'jquery',
+
+        'windows.jQuery': 'jquery'
+
+      })
+
+    ]
+  }
+}
